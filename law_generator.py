35596c6b3a1234e03fffb282074385856474cc04
Gabriel Kabbe <gabriel.kabbe@mail.de>
2018-04-28 17:32:28 +0200

    update

--- law_generator.py
+++ law_generator.py
@@ -1,6 +1,6 @@
 import fire
 import numpy as np
-import tensorflow as tf
+from tensorflow import keras
 import torch
 from torch.utils.data import Dataset, DataLoader
 
@@ -9,7 +9,7 @@ class CharPredictor(torch.nn.Module):
     def __init__(self, number_of_chars):
         super().__init__()
         self.lstm = torch.nn.LSTM(number_of_chars, number_of_chars)
-        self.softmax = torch.nn.Softmax(dim=-1)
+        self.softmax = torch.nn.LogSoftmax(dim=-1)
 
 
     def forward(self, x, hidden):
@@ -24,7 +24,7 @@ def get_text(filename):
 
 
 def text_to_onehot(text):
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level=True)
+    tokenizer = keras.preprocessing.text.Tokenizer(char_level=True)
     tokenizer.fit_on_texts(text)
     text_vec = tokenizer.texts_to_matrix(text).astype(np.float32)
     reverse_dict = {v: k for k, v in tokenizer.word_index.items()}
@@ -49,6 +49,14 @@ class TextDataset(Dataset):
         return self.data[item]
 
 
+def feed_sequence(net, hidden, cell, sequence, criterion):
+    loss = 0
+    for in_, expected in zip(sequence[:-1], sequence[1:]):
+        output, (hidden, cell) = net(in_[None, ...], (hidden, cell))
+        loss += criterion(output[0], torch.argmax(expected, dim=-1))
+    return loss
+
+
 def main():
     dataset = TextDataset("bgb.md")
     number_of_chars = dataset[0].shape[1]
@@ -58,9 +66,15 @@ def main():
                     torch.zeros(1, 1, number_of_chars))
 
     # print(lstm(dataset[:1]))  # optional: , (hidden, cell)))
-    cp = CharPredictor(number_of_chars)
+    net = CharPredictor(number_of_chars)
 
-    print(cp(dataset[:1], (hidden, cell)))
+    optimizer = torch.optim.SGD(params=net.parameters(), lr=1e-4)
+    criterion = torch.nn.NLLLoss()
+
+    output, (hidden, cell) = net(dataset[:1], (hidden, cell))
+
+    loss = feed_sequence(net, hidden, cell, dataset[:10], criterion)
+    print(loss)
 
 
 fire.Fire()
